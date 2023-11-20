adc9f7210e3b32da42d948d3d278fbe4c86e9693
eetu kapanen <eetu.kapanen@tuni.fi>
2023-11-20 21:31:36 +0200

    isempthy

---
+++ tests/isEmpthy.test.js
@@ -0,0 +1,21 @@
+import isEmpty from '../../src/isEmpty.js';
+
+test('tyhjällä arvolla saadaan arvon totuus arvo true', () => {
+    expect(isEmpty().toBe(true));
+});
+
+test('totuus arvolla true saadaan arvo true', () => {
+    expect(isEmpty(true).toBe(true));
+});
+
+test('boolean arvolla 1 saadaan true', () => {
+    expect(isEmpty(1).toBe(true));
+});
+
+test('array arvolla saadaan arvoksi false', () => {
+    expect(isEmpty([1, 2, 3]).toBe(false));
+});
+
+test('stringit antavat arvon false', () => {
+    expect(isEmpty('abc').toBe)
+})
